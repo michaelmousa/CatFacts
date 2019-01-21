751037b348eb79d7a1271452c2d197bc5a7f0a30
michael <ghaith.mousa@gmail.com>
2019-01-21 16:57:33 +0000

    Initial commit

---
+++ app/src/main/java/com/treehouse/randomjoke/di/AppComponent.kt
@@ -0,0 +1,12 @@
+package com.treehouse.randomjoke.di
+
+
+import com.treehouse.randomjoke.ui.home.MainActivity
+import dagger.Component
+import javax.inject.Singleton
+
+@Component(modules = [NetworkModule::class])
+@Singleton
+interface AppComponent {
+    fun inject(mainActivity: MainActivity)
+}
